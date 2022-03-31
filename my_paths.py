5e060055cd46d707e88614ae2ec49b53bd548b5f
Alastair R Tanner <8190834+altanner@users.noreply.github.com>
2022-03-31 21:04:06 +0100

    Create my_paths.py

---
+++ my_paths.py
@@ -0,0 +1,8 @@
+udp = "YOUR UDP FOLDER PATH"
+tcp = "YOUR TCP FOLDER PATH"
+auth = "YOUR OVPN AUTH KEY"
+
+# for example
+# udp = "/home/bob/ovpn/udp/"
+# tcp = "/home/bob/ovpn/tcp/"
+# auth = "/home/bob/ovpn/auth"
